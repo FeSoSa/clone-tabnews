d117b1825d48d374b1cc293f28d8c5672e72e6e6
Felipe <felipesouzasalles@gmail.com>
2026-08-10 19:36:07 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "One Piece é REAL!" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenConnectionValue =
+    databaseOpenConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    dependencies: {
+      database: {
+        updated_at: updatedAt,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        open_connections: databaseOpenConnectionValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
